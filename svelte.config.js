13c2c0486da04df0117ba475708eff477aab8214
usepgnwan <usepgnwan@gmail.com>
2025-04-25 15:52:47 +0700

    haps

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html',
+			pages: 'build',  // Pastikan output ke build/
+			assets: 'build', // Pastikan assets ke build/
+			precompress: false
+		  }),
 		paths: {
 			// base: dev ? '' : process.env.BASE_PATH,
 			base:''
