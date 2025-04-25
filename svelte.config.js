184a59d84486c8330457996109f20ea984cd1132
usepgnwan <usepgnwan@gmail.com>
2025-04-25 16:03:52 +0700

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-const dev = process.argv.includes('dev');
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+  preprocess: vitePreprocess(),
+  kit: {
 		adapter: adapter({
-			fallback: 'index.html',
-			pages: 'build',  // Pastikan output ke build/
-			assets: 'build', // Pastikan assets ke build/
-			precompress: false
-		  }),
+		fallback: 'index.html',
+		pages: 'build',
+		assets: 'build',
+		precompress: false
+		}),
 		paths: {
-			// base: dev ? '' : process.env.BASE_PATH,
-			base:''
+		base: '' // HARUS kosong untuk username.github.io
 		}
-	}
+  }
 };
 
 export default config;
