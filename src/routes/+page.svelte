b6fce609ca6f460dec422e146612406f413dfb7b
usepgnwan <usepgnwan@gmail.com>
2025-04-25 15:20:05 +0700

    svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="/tes"  data-sveltekit-preload-data="tap">klik me</a> </p>
